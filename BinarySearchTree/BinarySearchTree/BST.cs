0d9e239a885e38aa06f1db042ab05066cca8f887
judgegrubb <elijah.grubb@utah.edu>
2015-09-14 18:27:02 -0600

    Created bst class and implemented node
    will finish bst implementation later

---
+++ BinarySearchTree/BinarySearchTree/BST.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchTree
+{
+    public class BST<T>
+    {
+        private class Node<E>
+        {
+            Node<E> left { get; set; }
+            Node<E> right { get; set; }
+            E data { get; set; }
+
+            public Node(E d)
+            {
+                data = d;
+                left = null;
+                right = null;
+            }
+    }
+}
